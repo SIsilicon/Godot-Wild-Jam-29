401f3f8a160d3a8de29d65cca61eae7daaa2b022
IAmNotIsaac <isaacjulianastell@gmail.com>
2021-01-12 18:56:22 -0800

    Door and pressure pad tweaks

--- src/logic/pressure_pad.gd
+++ src/logic/pressure_pad.gd
@@ -2,4 +2,28 @@ extends InputDevice
 
 
 
+var pushers : Array = []
+
+
 # TODO: Add player interactibility.
+
+
+func update_pushers() -> void:
+	if len(pushers) > 0:
+		set_state(States.ENABLED)
+	else:
+		set_state(States.DISABLED)
+
+
+
+func _on_PressureArea_body_entered(body : PhysicsBody) -> void:
+	if body.is_in_group("pushes_pressure_pad"):
+		pushers.append(body)
+		update_pushers()
+
+
+
+func _on_PressureArea_body_exited(body : PhysicsBody) -> void:
+	if body.is_in_group("pushes_pressure_pad"):
+		pushers.erase(body)
+		update_pushers()
