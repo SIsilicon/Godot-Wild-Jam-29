6192c48303bc3f31d241f9105d56787aa0e2a0df
JestemStefan <37214990+JestemStefan@users.noreply.github.com>
2021-01-14 00:52:33 +0100

    Added animation and tweeks to character controler + tested logic devices

--- src/entities/Cloud.gd
+++ src/entities/Cloud.gd
@@ -2,8 +2,6 @@ class_name Cloud
 extends KinematicBody
 
 var velocity: Vector3 = Vector3.ZERO
-const PULL_SPEED = 5
-const SHOOT_SPEED = 20
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,13 +12,13 @@ func _physics_process(delta):
 	move_and_slide(velocity)
 	
 	
-func pull(direction: Vector3):
+func pull(direction: Vector3, pull_strength: int):
 	
 	var pull_direction = global_transform.origin.direction_to(direction)
 	
-	velocity = pull_direction * PULL_SPEED
+	velocity = pull_direction * pull_strength
 	
-func shoot(push_direction: Vector3):
-	
-	velocity = push_direction * SHOOT_SPEED
+func shoot(push_direction: Vector3, shoot_speed: int):
+	randomize()
+	velocity = (push_direction + Vector3(rand_range(-0.1,0.1), rand_range(-0.1,0.1), 0)) * shoot_speed
 
